49156378c439a18e4144105744981d0334c7bbec
aehyemin <hsy13467926@gmail.com>
2024-07-07 22:16:42 +0900

    2575.py

---
+++ w1/Python/1978.py
@@ -0,0 +1,20 @@
+N = int(input()) 
+num_list = list(map(int, input().split()))
+prime = 0
+
+for num in num_list:
+     if num == 1:
+          continue
+     for i in range(2,num): #2부터 num-1까지 범위에서
+            if num % i == 0 :
+                     break
+            else:
+                  prime += 1
+
+
+print(prime)
+
+
+
+
+
