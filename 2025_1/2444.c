5530e3b98724b00775a2a97ad57de7be392288a4
aehyemin <hsy13467926@gmail.com>
2025-01-28 19:32:44 +0900

    2444.c

--- 2025_1/2444.c
+++ 2025_1/2444.c
@@ -4,9 +4,35 @@ int main ( ) {
     int n=0;
     scanf("%d", &n);
 
-    for(int i=0; i<2*n-1; i++) {
-        for(int j=0; j<n-1; j++) {
+    for(int i=0; i<n; i++) {
+
+        for(int j=0; j<n-i-1; j++) {
             printf(" "); //공백개수 
         }
+        
+        for(int k=0; k<2*i+1; k++) {
+            printf("*");
+        }
+
+        // for(int j=0; j<n-1; j++) {
+        //     printf(" "); //공백개수 
+        // }
+        printf("\n");
+    }
+
+    for(int i=0; i<n-1; i++) {
+
+        for(int j=n; j>n-i-1; j--) {
+            printf(" "); //공백개수 
+        }
+
+        for(int k=2*n-1; k>2*(i+1); k--) {
+            printf("*");
+        }
+
+        // for(int j=0; j<n-1; j++) {
+        //     printf(" "); //공백개수 
+        // }
+        printf("\n");
     }
 }
