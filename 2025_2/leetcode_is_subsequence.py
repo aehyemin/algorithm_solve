e88596777d3daa9805813919daa0acaa05b33f2d
aehyemin <hsy13467926@gmail.com>
2025-02-14 00:27:45 +0900

    leetcode_is_subsequence

---
+++ 2025_2/leetcode_is_subsequence.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        i = j = 0
+        while i < len(s) and j < len(t):
+            if t[j] == s[i]:
+                i+=1
+                j+=1
+            elif t[j] != s[i]:
+                j+=1
+
+        if i == len(s):
+            return True
+        else:
+            return False
+     
+​
