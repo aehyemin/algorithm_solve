e669dadb4abf0c72c9704edf4e7d2724b2b8d277
aehyemin <hsy13467926@gmail.com>
2025-02-16 02:40:58 +0900

    lc_reverse_string1

---
+++ 2025_2/lc_reverse_string1.py
@@ -0,0 +1,7 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        n = len(s)
+        for i in range(n//2):
+            tmp = s[i]
+            s[i] = s[n-i-1]
+            s[n-i-1] = tmp
