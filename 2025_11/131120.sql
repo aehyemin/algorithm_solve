0deb6df66663bb30bd4043b7f5af3630e587570f
aehyemin <108813741+aehyemin@users.noreply.github.com>
2025-11-06 02:39:19 +0900

    Create 131120.sql

---
+++ 2025_11/131120.sql
@@ -0,0 +1,4 @@
+select MEMBER_ID, MEMBER_NAME, GENDER, date_format(DATE_OF_BIRTH, '%Y-%m-%d') as DATE_OF_BIRTH
+from member_profile
+where gender = 'W' and month(date_of_birth) = '3' and tlno is not null
+order by member_id
