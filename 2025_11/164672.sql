8ac46c7b7b55b436cd270f3a13483b6b5df99ee2
aehyemin <108813741+aehyemin@users.noreply.github.com>
2025-11-28 23:37:56 +0900

    164672.sql

---
+++ 2025_11/164672.sql
@@ -0,0 +1,9 @@
+select BOARD_ID,WRITER_ID,TITLE,PRICE, 
+    case 
+        when status = 'DONE' then '거래완료'
+        when status = 'RESERVED' then '예약중'
+        else '판매중'
+    end as STATUS
+from used_goods_board
+where created_date = '2022-10-05'
+order by board_id desc
